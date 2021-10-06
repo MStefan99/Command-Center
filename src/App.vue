21b68a8ebb6857bcedfcd353bbed757bb0807e4b
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-10-06 10:58:38 +0300

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,23 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="pug">
+NavBar
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {
+		NavBar
+	}
+};
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+html
+	font-family sans-serif
+
+	body
+		margin 0;
+		background-color #fcfeff
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+nav
+	a#home-link(href="/") Command Center
+	span#device-status(class={connected: device}) {{device? 'Connected' : 'Not connected'}}
+</template>
+
+
+<script>
+export default {
+	data() {
+		return {
+			device: {
+				deviceType: 'transceiver',
+				deviceID: 1
+			}
+		};
+	}
+};
+</script>
+
+
+<style scoped lang="stylus">
+nav
+	display flex
+	flex-flow row wrap
+	justify-content space-between
+	align-items center
+	background-color #555
+	color white
+	padding .5em
+	margin-bottom 1em
+
+	a
+		color inherit
+		text-decoration none
+
+		&#home-link
+			font-size 2em
+			font-weight bold
+
+	#device-status
+		font-weight bold
+		cursor pointer
+
+		&.connected
+			color #00f090
+</style>
