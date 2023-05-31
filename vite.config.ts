5cf08b5d6c79e4772b5b53adfcbe4c22284fab7d
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2023-05-31 17:50:56 +0000

    added husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import pugPlugin from 'vite-plugin-pug';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
-		port: 3006,
+		port: 3008,
 		host: '0.0.0.0'
 	},
-	plugins: [vue(), pugPlugin()]
+	plugins: [vue()]
 });
