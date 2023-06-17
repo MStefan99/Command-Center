51e4d1161382aeb4f92a688e5e2bbb9dfdff1b6c
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2023-06-17 11:16:34 +0300

    added io mixer ui
    updated dependencied

--- src/components/IOMixer.vue
+++ src/components/IOMixer.vue
@@ -7,22 +7,52 @@
 		option(value="500")
 		option(value="1000")
 	table
-		thead
-			tr(v-for="i in inputNumber + 1" :key="i")
-				th(v-for="j in inputNumber + 1" :key="j")
-					InputSlider(
-						type="range"
-						listID="stops"
-						:modelValue="i === j ? 1000 : 0"
-						:min="-1500"
-						:max="1500")
+		tbody
+			tr
+				td(v-for="(input, i) in inputs" :key="i")
+					InputSlider(type="range" listID="stops" v-model="inputs[i]")
+	.text *
+	.flex.flex-row.items-stretch
+		table
+			tbody
+				tr(v-for="(row, i) in mixes" :key="i")
+					td(v-for="(value, j) in row" :key="j")
+						InputSlider(type="range" listID="stops" v-model="mixes[i][j]")
+		.text +
+		table
+			tbody
+				tr(v-for="(trim, i) in trims" :key="i")
+					td
+						InputSlider(type="range" listID="stops" v-model="trims[i]")
+		.text =
+		table
+			tbody
+				tr(v-for="(output, i) in outputs" :key="i")
+					td
+						InputSlider(type="range" listID="stops" v-model="outputs[i]")
 </template>
 
 <script setup lang="ts">
 import InputSlider from './InputSlider.vue';
+import {ref} from 'vue';
 
 const inputNumber = 8;
 const outputNumber = 8;
+
+const inputs = ref<number[]>(new Array<number>(inputNumber).fill(0));
+const mixes = ref<number[][]>(
+	Array.from({length: outputNumber}, () => new Array<number>(inputNumber).fill(0))
+);
+const trims = ref<number[]>(new Array<number>(outputNumber).fill(0));
+const outputs = ref<number[]>(new Array<number>(outputNumber).fill(0));
 </script>
 
-<style scoped></style>
+<style scoped>
+.text {
+	@apply m-2;
+
+	align-self: center;
+	font-size: 3em;
+	font-weight: bold;
+}
+</style>
--- src/components/InputSlider.vue
+++ src/components/InputSlider.vue
@@ -1,35 +1,43 @@
 <template lang="pug">
 .slider
-	datalist(:id="'stops-' + idString" v-if="list")
-		option(
-			v-for="stop in list"
-			:key="stop"
-			:value="stop"
-			@change="$emit('update:modelValue', value)")
+	datalist(v-if="list" :id="'stops-' + idString")
+		option(v-for="stop in list" :key="stop" :value="stop")
 	.bar
 		input(
 			type="range"
-			:list="list ? 'stops-' + idString : listID"
-			:class="vertical && 'vertical'"
 			v-model="value"
+			@change="$emit('update:modelValue', +value)"
+			:list="list ? 'stops-' + idString : listID"
 			:min="min"
 			:max="max")
 		.track
-		.value {{value}}
+		.value.short {{(value / shortScalar ?? 1).toFixed(1)}}
+		.value.long {{value}}
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from 'vue';
+import {computed, ref, watch} from 'vue';
 
-const props = defineProps<{
-	modelValue: number;
-	vertical?: boolean;
-	min?: number;
-	max?: number;
-	list?: number[];
-	listID?: string;
-}>();
+const props = withDefaults(
+	defineProps<{
+		modelValue: number;
+		min?: number;
+		max?: number;
+		shortScalar?: number;
+		list?: number[];
+		listID?: string;
+	}>(),
+	{
+		min: -1500,
+		max: 1500,
+		shortScalar: 1000
+	}
+);
 defineEmits<{(e: 'update:modelValue', value: number): void}>();
+watch(
+	() => props.modelValue,
+	() => (value.value = props.modelValue)
+);
 
 const value = ref<number>(props.modelValue ?? 0);
 const percentage = computed(() => (value.value - props.min) / (props.max - props.min));
@@ -42,6 +50,19 @@ const idString = Array.from(id, (byte) => byte.toString(16).padStart(2, '0')).jo
 <style scoped>
 .bar {
 	position: relative;
+	margin: -8px 0;
+	--width: 3ch;
+	--height: 50px;
+}
+
+.bar:not(:focus-within) .long,
+.bar:focus-within .short {
+	visibility: hidden;
+}
+
+.bar:focus-within {
+	--width: 8ch;
+	--height: 200px;
 }
 
 .bar:after {
@@ -58,15 +79,11 @@ const idString = Array.from(id, (byte) => byte.toString(16).padStart(2, '0')).jo
 
 .bar input[type='range'] {
 	vertical-align: middle;
-	height: 50px;
-	width: 5ch;
+	height: var(--height);
+	width: var(--width);
 	-webkit-appearance: slider-vertical;
 	opacity: 0;
-	transition: height 0.5s ease;
-}
-
-.bar input[type='range']:focus {
-	height: 200px;
+	transition: height 0.5s ease, width 0.5s ease;
 }
 
 .track {
@@ -76,13 +93,15 @@ const idString = Array.from(id, (byte) => byte.toString(16).padStart(2, '0')).jo
 	left: 0;
 	bottom: 8px;
 	height: max(8px, calc((100% - 16px) * v-bind(percentage)));
-	width: 5ch;
+	width: 100%;
 	background-color: var(--color-accent);
+	transition: width 0.5s ease;
 }
 
 .value {
 	position: absolute;
 	pointer-events: none;
+	font-weight: bold;
 	left: 50%;
 	top: 50%;
 	transform: translate(-50%, -50%);
