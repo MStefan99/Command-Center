67033d9066ab8c48256ced9c82567fca6896051d
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2023-07-05 08:38:39 +0300

    removed usb check for demo

--- src/components/DeviceSelector.vue
+++ src/components/DeviceSelector.vue
@@ -1,25 +1,23 @@
 <template lang="pug">
 .popup-wrapper(@click.self="$emit('close')")
 	.device-selector
-		div(v-if="usbAvailable")
-			span.bold.block(v-if="connectedDevices.length") Connected devices
-			span.bold.block(v-else) No devices connected
-			.device(v-for="device of connectedDevices" :key="device.id")
-				span.cursor-pointer(@click="viewedDevice = device") {{device.productName}}
-				.flex.flex-row.flex-wrap.gap-4
-					button.green-outline.grow(v-if="device.is(activeDevice)") Active device
-					button.green.grow(v-else @click="activeDevice = device") Set active
-					button.red.grow(@click="device.disconnect()") Disconnect
-			span.bold.block.my-2 Connect
-			button.accent.bold.w-full(@click="connect()")
-				| {{connectedDevices.length ? 'Connect another' : 'Connect'}}
-			button.bold.accent-outline.w-full.mt-2(@click="connect(true)") Connect demo device
-		.no-usb(v-else)
-			p.text-red Unfortunately, WebUSB is unavailable on this page.
-			p.text-red This might happen because you are using an older browser that doesn't support WebUSB
-				|
-				| or you opened this page using an insecure HTTP connection.
-			p.text-red Please ensure you are using a secure connection or try another browser and/or device.
+		span.bold.block(v-if="connectedDevices.length") Connected devices
+		span.bold.block(v-else) No devices connected
+		.device(v-for="device of connectedDevices" :key="device.id")
+			span.cursor-pointer(@click="viewedDevice = device") {{device.productName}}
+			.flex.flex-row.flex-wrap.gap-4
+				button.green-outline.grow(v-if="device.is(activeDevice)") Active device
+				button.green.grow(v-else @click="activeDevice = device") Set active
+				button.red.grow(@click="device.disconnect()") Disconnect
+		span.bold.block.my-2 Connect
+		button.accent.bold.w-full(v-if="usbAvailable" @click="connect()")
+			| {{connectedDevices.length ? 'Connect another' : 'Connect'}}
+		button.bold.accent-outline.w-full.mt-2(@click="connect(true)") Connect demo device
+		p.text-red.mt-4(v-if="!usbAvailable").
+			Unfortunately, WebUSB is unavailable on this page.
+			This might happen because you are using an browser that doesn't support WebUSB
+			or you opened this page using an insecure HTTP connection.
+			Please ensure you are using a secure connection or try another browser and/or device.
 	Transition
 		DeviceViewer(v-if="viewedDevice" :device="viewedDevice" @close="viewedDevice = null")
 </template>
